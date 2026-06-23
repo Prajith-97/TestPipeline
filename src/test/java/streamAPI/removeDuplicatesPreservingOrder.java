81e3c7175324abe7df0b5d1805461c240bc80887
Prajith-97 <tprajith70@gmail.com>
2026-06-23 16:22:19 +0530

    test1

--- src/test/java/streamAPI/removeDuplicatesPreservingOrder.java
+++ src/test/java/streamAPI/removeDuplicatesPreservingOrder.java
@@ -5,8 +5,7 @@ import java.util.stream.Collectors;
 
 public class removeDuplicatesPreservingOrder {
     List<Integer> arrList = new ArrayList<>(Arrays.asList(2, 3, 1, 2, 3, 4, 5, 6, 7, 8, 7, 10, 11));
-    List<Integer>array = arrList.stream()
-            .sorted(Comparator.comparing())
-            .collect(Collectors
-                    LinkedList::new;
+    List<Integer>array = arrList.stream().distinct()
+            .collect(Collectors.toList());
+
 }
